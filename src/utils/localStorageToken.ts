e048b02169da8a252dc43ab8eca744cbaa7232d4
Ali Abdalrhman <ali.abdalrhman.606@gmail.com>
2024-07-01 19:53:28 +0300

    Add localstorage access functions

---
+++ src/utils/localStorageToken.ts
@@ -0,0 +1,13 @@
+const TOKEN_KEY = "sal_token";
+
+function getLocalStorageToken() {
+  return localStorage.getItem(TOKEN_KEY);
+}
+function setLocalStorageToken(token: string) {
+  return localStorage.setItem(TOKEN_KEY, token);
+}
+function hasLocalStorageToken() {
+  return !!getLocalStorageToken;
+}
+
+export { getLocalStorageToken, setLocalStorageToken, hasLocalStorageToken };
