e3483d3da25a29a2a3ab6bb889c6b0868aa3649f
Ali Abdalrhman <ali.abdalrhman.606@gmail.com>
2024-07-01 19:43:49 +0300

    Add Router

--- src/router.tsx
+++ src/router.tsx
@@ -4,7 +4,15 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import Login from "./pages/Login";
+import AuthGaurd from "./components/gaurds/AuthGaurd";
 
 export const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/login" element={<Login />} />)
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<AuthGaurd />}>
+        <Route index element={<h1>Home</h1>} />
+        <Route path="/login" element={<Login />} />
+      </Route>
+    </>
+  )
 );
