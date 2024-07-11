d0684c32f9deda4dc5df73b6682a152c271e1d7b
Ali Abdalrhman <ali.abdalrhman.606@gmail.com>
2024-07-11 17:08:05 +0300

    Add Login Form

---
+++ src/components/forms/LoginForm.tsx
@@ -0,0 +1,55 @@
+import { Box, Button, FormControl, Input } from "@chakra-ui/react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+import useLogin from "../../hooks/useLogin";
+
+const schema = yup.object({
+  username: yup.string().required(),
+  password: yup.string().required(),
+});
+
+type LoginData = yup.InferType<typeof schema>;
+
+export default function LoginForm() {
+  const { mutate: login, isPending } = useLogin();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<LoginData>({ resolver: yupResolver(schema) });
+
+  const onSubmit: SubmitHandler<LoginData> = (data) => {
+    login(data);
+  };
+
+  return (
+    <>
+      <Box
+        as="form"
+        py="20"
+        maxW="md"
+        mx={"auto"}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <FormControl isInvalid={!!errors?.username}>
+          <Input placeholder="username" {...register("username")} />
+        </FormControl>
+        <FormControl isInvalid={!!errors?.password}>
+          <Input
+            placeholder="Password"
+            type="password"
+            {...register("password")}
+          />
+        </FormControl>
+
+        <Button type="submit" isLoading={isPending}>
+          Login
+        </Button>
+      </Box>
+
+      {/* <Button colorScheme="blue">Button</Button>
+      <Input border="2px" /> */}
+    </>
+  );
+}
