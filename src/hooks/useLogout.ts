bd4617774489ddc16210591f0b2b3fdde8334573
Ali Abdalrhman <ali.abdalrhman.606@gmail.com>
2024-07-16 01:03:01 +0300

    Add Logout functionality

---
+++ src/hooks/useLogout.ts
@@ -0,0 +1,22 @@
+import { useMutation } from "@tanstack/react-query";
+import axiosInstance from "../api/index";
+import useAuthContext from "./useAuthContext";
+
+function logout() {
+  return axiosInstance.post<LoginResponse>("/logout");
+}
+
+const useLogout = () => {
+  //   const navigate = useNavigate();
+  const { onLogout } = useAuthContext();
+
+  return useMutation({
+    mutationKey: ["login"],
+    mutationFn: async () => {
+      onLogout();
+      logout();
+    },
+  });
+};
+
+export default useLogout;
