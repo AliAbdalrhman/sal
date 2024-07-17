595de79306fc80d07eaa9679a43c57125691db84
Ali Abdalrhman <ali.abdalrhman.606@gmail.com>
2024-07-17 21:17:25 +0300

    Add Header icons

---
+++ src/components/icons/NotificationIcon.tsx
@@ -0,0 +1,15 @@
+import { createIcon } from "@chakra-ui/icon";
+export const Notification = createIcon({
+  displayName: "Notification",
+  viewBox: "0 0 33 33",
+  path: [
+    <path
+      d="M26.8051 10.3059C26.8051 4.61497 22.1901 0 16.4992 0C10.8083 0 6.1933 4.61497 6.1933 10.3059V20.6117L0.00976562 24.7341V26.7953H32.9886V24.7341L26.8051 20.6117V10.3059Z"
+      fill="currentColor"
+    />,
+    <path
+      d="M20.6217 28.8564H12.377C12.377 31.134 14.2217 32.9788 16.4993 32.9788C18.7769 32.9788 20.6217 31.134 20.6217 28.8564Z"
+      fill="currentColor"
+    />,
+  ],
+});
